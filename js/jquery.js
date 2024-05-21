5ad0200239a582fb6558f3ae7f4323e056e856a5
jordanbarraduran <jor.barra@duocuc.cl>
2024-05-21 19:54:12 -0400

    primer commit

---
+++ js/jquery.js
@@ -0,0 +1,32 @@
+// Previene el comportamiento por defecto de los enlaces
+$(".nav-link").on( "click", function( event ) {
+    event.preventDefault();
+    console.log("Prevenido");
+
+    // Función para cargar contenido
+    function cargarContenido(archivo) {
+        $("#contenido").load(archivo);
+        console.log("Cargando " + archivo);
+    }
+
+    // Carga el contenido de la página correspondiente en el div con id="contenido"
+    $("#link-maceteros").click(function(e){
+        e.preventDefault();
+        cargarContenido('maceteros.html');
+    });
+
+    $("#link-sustratos").click(function(e){
+        e.preventDefault();
+        cargarContenido('sustratos.html');
+    });
+
+    $("#link-flores").click(function(e){
+        e.preventDefault();
+        cargarContenido('flores.html');
+    });
+
+    $("#link-arbustos").click(function(e){
+        e.preventDefault();
+        cargarContenido('arbustos.html');
+    });
+});
