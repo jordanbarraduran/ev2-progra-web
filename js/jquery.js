c9d0008289e86de40ab9a4b2b8e5a86f1724278e
jordanbarraduran <jor.barra@duocuc.cl>
2024-05-22 18:45:36 -0400

    cambios

--- js/jquery.js
+++ js/jquery.js
@@ -1,6 +1,4 @@
 // Previene el comportamiento por defecto de los enlaces
-var carrito = [];
-
 $(".nav-link").on( "click", function( event ) {
     event.preventDefault();
     console.log("Prevenido");
@@ -36,50 +34,14 @@ $(".nav-link").on( "click", function( event ) {
         e.preventDefault();
         cargarContenido('carrito.html');
     });
-});
 
-// Función para mostrar el contenido del carrito
-function mostrarCarrito() {
-    // Limpiar lista de carrito
-    $('#lista-carrito').empty();
-
-    // Calcular total del carrito
-    var total = 0;
-
-    // Iterar sobre los productos en el carrito
-    $.each(carrito, function(index, producto) {
-        // Agregar producto a la lista del carrito
-        $('#lista-carrito').append('<li>' + producto.nombre + ' - $' + producto.precio.toFixed(2) + '</li>');
-        
-        // Sumar el precio del producto al total
-        total += producto.precio;
+    $("#link-iniciar-sesion").click(function(e){
+        e.preventDefault();
+        cargarContenido('iniciosesion.html');
     });
 
-    // Mostrar total del carrito
-    $('#total-carrito').text('$' + total.toFixed(2));
-}
-
-$(".agregar-carrito").on( "click", function( event ) {
-    console.log("Agregando a carrito");
-
-    $("#agregar-carrito").click(function(e){
-        mostrarCarrito();
+    $("#link-registrarse").click(function(e){
+        e.preventDefault();
+        cargarContenido('registro.html');
     });
 });
-// Añade productos al carrito (probando con arbustos.html)
-
-// Manejar clic en botón "Agregar al carrito"
-$('#agregarCarrito').click(function(e) {
-    console.log("Agregado al carrito");
-    
-    // Obtener información del producto desde el atributo data
-    var nombre = $(this).data('nombre');
-    var precio = $(this).data('precio');
-    
-    // Agregar producto al carrito
-    carrito.push({ nombre: nombre, precio: precio });
-
-    // Actualizar visualización del carrito
-    mostrarCarrito();
-  });
-
